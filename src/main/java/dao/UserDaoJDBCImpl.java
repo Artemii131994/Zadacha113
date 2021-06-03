b110cfddd19c0a0d5b6efb708924c44e1f435eab
Artemii131994 <artemii131994@gmail.com>
2021-06-03 11:39:15 +0400

    подправил

--- src/main/java/dao/UserDaoJDBCImpl.java
+++ src/main/java/dao/UserDaoJDBCImpl.java
@@ -1,62 +1,57 @@
 package dao;
-
 import model.User;
 import util.Util;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-
 public class UserDaoJDBCImpl implements UserDao {
+
+    private static final Connection connection = Util.getConnection();
 
     public UserDaoJDBCImpl() {
     }
-
     public void createUsersTable() { // создание таблицы
-        try (Statement statement = Util.getConnection().createStatement()) {
+        try (Statement statement = connection.createStatement()) {
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS testrt" +
                     "(id int not null auto_increment," +
                     " name VARCHAR(50), " +
                     "lastname VARCHAR(50), " +
                     "age int, " +
                     "PRIMARY KEY (id))");
-        } catch (SQLException | ClassNotFoundException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-
     public void dropUsersTable() { //удаление таблицы
-        try (Statement statement = Util.getConnection().createStatement();) {
+        try (Statement statement = connection.createStatement()) {
             statement.executeUpdate("DROP TABLE IF EXISTS testrt");
-        } catch (SQLException | ClassNotFoundException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-
     public void saveUser(String name, String lastName, byte age) { // сохранение user в таблицы
-        try (PreparedStatement prepaSt = Util.getConnection().prepareStatement("INSERT INTO testrt(name, lastName, age) VALUES (?, ?, ?)");) {
+        try (PreparedStatement prepaSt = connection
+                .prepareStatement("INSERT INTO testrt(name, lastName, age) VALUES (?, ?, ?)")) {
             prepaSt.setString(1, name);
             prepaSt.setString(2, lastName);
             prepaSt.setString(3, String.valueOf(age));
             prepaSt.executeUpdate();
             System.out.println("User с именем " + name + " добавлен базу данных");
-        } catch (SQLException | ClassNotFoundException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-
     public void removeUserById(long id) { //удаление по id
-        try (PreparedStatement prSt = Util.getConnection().prepareStatement("DELETE FROM testrt WHERE id=?");) {
+        try (PreparedStatement prSt = connection.prepareStatement("DELETE FROM testrt WHERE id=?");) {
             prSt.setLong(1, id);
-        } catch (SQLException | ClassNotFoundException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-
     public List<User> getAllUsers() { // вывод всех user
         List<User> user = new ArrayList<>();
-        try (ResultSet res = Util.getConnection().createStatement().executeQuery("SELECT * FROM testrt")) {
-
+        try (Statement statement = connection.createStatement();
+             ResultSet res = statement.executeQuery("SELECT * FROM testrt")) {
             while (res.next()) {
                 User user1 = new User();
                 user1.setId(res.getLong("id"));
@@ -65,18 +60,15 @@ public class UserDaoJDBCImpl implements UserDao {
                 user1.setAge(res.getByte("age"));
                 user.add(user1);
             }
-
-        } catch (SQLException | ClassNotFoundException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
-            ;
         }
         return user;
     }
-
     public void cleanUsersTable() { // очистка таблицы
-        try (Statement statement = Util.getConnection().createStatement()) {
+        try (Statement statement = connection.createStatement()) {
             statement.executeUpdate("TRUNCATE TABLE testrt");
-        } catch (SQLException | ClassNotFoundException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -1,28 +1,22 @@
 package util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Util {
 
-    public static Connection getConnection() throws SQLException, ClassNotFoundException {
-        String hostName = "localhost";
-        String dbName = "test";
-        String userName = "root";
-        String password = "root";
+    private static final String HOST = "jdbc:mysql://localhost:3307/test";
+    private static final String LOGIN = "root";
+    private static final String PASSWORD = "root";
 
-        return getConnection(hostName, dbName, userName, password);
-    }
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
 
-    public static Connection getConnection(String hostName, String dbName, String userName, String password)
-            throws SQLException, ClassNotFoundException {
-
-        Class.forName("com.mysql.jdbc.Driver");
-        String connectionURL = "jdbc:mysql://" + hostName + ":3307/"
-                + dbName;
-
-        return DriverManager.getConnection(connectionURL, userName, password);
+            connection = DriverManager.getConnection(HOST, LOGIN, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
     }
 
 }
